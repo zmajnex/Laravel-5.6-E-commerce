161bed93814f0c14dbf705ba18d159e4c2f7a114
zmajnex <zmajnex@yahoo.com>
2018-03-28 16:26:59 +0200

    products template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@ Home page
 {{--@section('login-msg')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-sm-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -33,4 +33,41 @@ Home page
       
 </div>
 
+      {{--Thumbnails--}}
+      <div class="container">
+            <div class="row ">
+              <div class="col-4">
+                    <img class="card-img-top" src="{{asset('storage/mobile.jpeg')}}" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                      <button class="btn btn-primary">KUPI</button>
+                    </div>
+              </div>
+              <div class="col-4">
+                    <img class="card-img-top" src="{{asset('storage/mobile.jpeg')}}" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                      <button class="btn btn-primary">KUPI</button>
+                    </div>
+              </div>
+              <div class="col-4">
+                    <img class="card-img-top" src="{{asset('storage/mobile.jpeg')}}" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                      <button class="btn btn-primary">KUPI</button>
+                    </div>
+              </div>
+              <div class="col-4 col ">
+                    <img class="card-img-top" src="{{asset('storage/mobile.jpeg')}}" alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                      <button class="btn btn-primary">KUPI</button>
+                    </div>
+              </div>
+            </div>
+          </div>
 @endsection
